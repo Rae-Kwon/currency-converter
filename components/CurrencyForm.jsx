4b544ea7528ef3bdaa3567cdedd89178152d475a
Richard Kwon <rkminjae@gmail.com>
2020-06-10 17:51:28 +1200

    Add CurrencyForm component

---
+++ components/CurrencyForm.jsx
@@ -0,0 +1,20 @@
+
+
+import React from 'react'
+
+const CurrencyForm = () => {
+    return (
+        <form>
+            <label htmlFor="">
+                <input type="text" name="" id=""/>
+            </label>
+            
+            <label htmlFor="">
+                <input type="text" name="" id=""/>
+            </label>
+            <input type="submit" value="Convert"/>
+        </form>
+    )
+}
+
+export default CurrencyForm
