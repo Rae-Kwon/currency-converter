aee44cd6d95cc7b5440a3bfbcbe511084726b396
Richard Kwon <rkminjae@gmail.com>
2020-06-10 18:14:57 +1200

    Add useCurrencyForm custom hook to handle change and submit

--- components/CurrencyForm.jsx
+++ components/CurrencyForm.jsx
@@ -1,16 +1,36 @@
+import React, { useState } from 'react'
 
+const useCurrencyForm = () => {
+    const [inputs, setInputs] = useState({})
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log("submitted")
+    }
 
-import React from 'react'
+    const handleInputChange = (event) => {
+        console.log(event.target.value)
+        event.persist()
+        setInputs(inputs => ({...inputs, [event.target.name]: event.target.value}))
+    }
+
+    return {
+        handleSubmit,
+        handleInputChange,
+        inputs
+    }
+}
 
 const CurrencyForm = () => {
+    const { inputs, handleSubmit, handleInputChange } = useCurrencyForm()
+    console.log(inputs)
     return (
-        <form>
-            <label htmlFor="">
-                <input type="text" name="" id=""/>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="baseCurrency">
+                <input type="number" name="baseCurrency" id="" onChange={handleInputChange}/>
             </label>
             
-            <label htmlFor="">
-                <input type="text" name="" id=""/>
+            <label htmlFor="convertedCurrency">
+                <input type="number" name="convertedCurrency" id="" onChange={handleInputChange}/>
             </label>
             <input type="submit" value="Convert"/>
         </form>
