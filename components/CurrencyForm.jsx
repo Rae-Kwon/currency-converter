f3bbcd86b1c194ec2d1ca4c9f28a25dc0dd0a7bf
Richard Kwon <rkminjae@gmail.com>
2020-06-12 16:14:40 +1200

    Convert currency based on user selected currency codes

--- components/CurrencyForm.jsx
+++ components/CurrencyForm.jsx
@@ -1,24 +1,37 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 
 import { useCurrencyForm } from '../hooks/customHooks'
+import { CurrencyContext } from '../CurrencyContext'
 
 
 const CurrencyForm = () => {
     const { inputs, handleSubmit, handleInputChange } = useCurrencyForm()
+    const [exchangeRate] = useContext(CurrencyContext)
+    const [resultCurrency, setResultCurrency] = useState(0)
+    if(exchangeRate.rates === undefined) return <div>Loading...</div>
+    if(exchangeRate.rates !== undefined) {
+        const conversionRates = Object.values(exchangeRate.rates)[0]
+
+        const convertCurrency = (event) => {
+            const conversionResult = (event.target.value * conversionRates).toFixed(2)
+            setResultCurrency(conversionResult)
+        }
+        console.log(inputs)
+
+        return (
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="baseCurrency">
+                    <input type="number" name="baseCurrency" placeholder="Enter amount" onChange={convertCurrency} />
+                </label>
+                
+                <label htmlFor="convertedCurrency">
+                    <input type="number" name="convertedCurrency" value={resultCurrency} readOnly/>
+                </label>
+            </form>
+        )
+    }
+
     
-    inputs.baseCurrency = '1'
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="baseCurrency">
-                <input type="number" name="baseCurrency" value={inputs.baseCurrency} onChange={handleInputChange}/>
-            </label>
-            
-            <label htmlFor="convertedCurrency">
-                <input type="number" name="convertedCurrency" id="" onChange={handleInputChange}/>
-            </label>
-            <input type="submit" value="Convert"/>
-        </form>
-    )
 }
 
 export default CurrencyForm
