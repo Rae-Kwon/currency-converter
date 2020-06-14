58142ca3c4391f75754650581364cda7bf59b0af
Richard Kwon <rkminjae@gmail.com>
2020-06-15 10:15:10 +1200

    Add resultCurrency and inputCurrency states to context

--- CurrencyContext.jsx
+++ CurrencyContext.jsx
@@ -2,11 +2,19 @@ import React, { useState, useEffect, createContext } from 'react'
 
 export const CurrencyContext = createContext()
 
-export const CurrencyProvider = ({ children, baseCurrencyCode, resultCurrencyCode }) => {
+export const CurrencyProvider = ({ children }) => {
     const [exchangeRates, setExchangeRates] = useState({})
+    const [resultCurrency, setResultCurrency] = useState(0)
+    const [inputCurrency, setInputCurrency] = useState(0)
     
     return (
-        <CurrencyContext.Provider value={[exchangeRates, setExchangeRates]}>
+        <CurrencyContext.Provider value={
+            {
+                exchange: [exchangeRates, setExchangeRates], 
+                result: [resultCurrency, setResultCurrency], 
+                input: [inputCurrency, setInputCurrency]}
+            }
+            >
             {children}
         </CurrencyContext.Provider>
     )
