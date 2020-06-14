e3487e12a72f43ba077ba36780b4b70e76a97288
Richard Kwon <rkminjae@gmail.com>
2020-06-15 10:17:08 +1200

    Remove unused handlers in custom hook

--- hooks/customHooks.js
+++ hooks/customHooks.js
@@ -2,20 +2,14 @@ import { useState } from 'react'
 
 export const useCurrencyForm = () => {
     const [inputs, setInputs] = useState({})
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        console.log("submitted")
-    }
 
     const handleInputChange = (event) => {
-        console.log(event.target.value)
         event.persist()
         setInputs(inputs => ({...inputs, [event.target.name]: event.target.value}))
     }
 
     return {
-        handleSubmit,
         handleInputChange,
-        inputs
+        inputs,
     }
 }
