b872114bb4cae58a24926c5d24eba7dcb830827a
Richard Kwon <rkminjae@gmail.com>
2020-06-16 16:43:18 +1200

    Add baseCode and resultCode states

--- CurrencyContext.jsx
+++ CurrencyContext.jsx
@@ -5,16 +5,20 @@ export const CurrencyContext = createContext()
 export const CurrencyProvider = ({ children }) => {
     const [exchangeRates, setExchangeRates] = useState({})
     const [resultCurrency, setResultCurrency] = useState(0)
-    const [inputCurrency, setInputCurrency] = useState(0)
+    const [inputCurrency, setInputCurrency] = useState(1)
+    const [baseCurrencyCode, setBaseCurrencyCode] = useState("NZD")
+    const [resultCurrencyCode, setResultCurrencyCode] = useState("USD")
     
     return (
         <CurrencyContext.Provider value={
-            {
-                exchange: [exchangeRates, setExchangeRates], 
-                result: [resultCurrency, setResultCurrency], 
-                input: [inputCurrency, setInputCurrency]}
-            }
-            >
+                {
+                    exchange: [exchangeRates, setExchangeRates], 
+                    result: [resultCurrency, setResultCurrency], 
+                    input: [inputCurrency, setInputCurrency],
+                    baseCode: [baseCurrencyCode, setBaseCurrencyCode],
+                    resultCode: [resultCurrencyCode, setResultCurrencyCode]
+                }
+            }>
             {children}
         </CurrencyContext.Provider>
     )
