2dc66f3906210fc327ed481b7aef9ca9bf6066b0
Richard Kwon <rkminjae@gmail.com>
2020-06-16 16:45:08 +1200

    Add currencyConversion customHook

--- hooks/customHooks.js
+++ hooks/customHooks.js
@@ -1,15 +1,25 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
+
+import { CurrencyContext } from '../CurrencyContext'
 
 export const useCurrencyForm = () => {
     const [inputs, setInputs] = useState({})
+    const { result } = useContext(CurrencyContext)
+    const [resultCurrency, setResultCurrency] = result
 
     const handleInputChange = (event) => {
         event.persist()
         setInputs(inputs => ({...inputs, [event.target.name]: event.target.value}))
     }
 
+    const convertCurrency = (inputCurrency, conversionRates) => {
+        const conversionResult = (inputCurrency * conversionRates).toFixed(2)
+        setResultCurrency(conversionResult)
+    }
+
     return {
         handleInputChange,
+        convertCurrency,
         inputs,
     }
 }
