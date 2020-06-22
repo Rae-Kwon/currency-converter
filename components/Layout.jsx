51043552adee3509006c89cb95fb05ca262557e8
Richard Kwon <rkminjae@gmail.com>
2020-06-23 11:58:42 +1200

    Add markup for navbar

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 
 const Layout = ({ children }) => {
     return (
-        <div>
-            <ul>
-                <li>Home</li>
-                <li>Currency History</li>
-            </ul>
+        <div className="layout-container">
+            <header>
+                <h1 className="logo">Logo</h1>
+                <input type="checkbox" id="nav-toggle" className="nav-toggle"/>
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Currency History</a></li>
+                    </ul>
+                </nav>
+                <label htmlFor="nav-toggle" className="nav-toggle-label">
+                    <span></span>
+                </label>
+            </header>
             {children}
         </div>
     )
